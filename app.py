bbdf62fe4253f12f715e84c2d85cd88cdd4be1de
OXRED9 <Alktroon-90@hotmail.com>
2026-05-17 21:04:10 +0300

    Test

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ class MyHandler(BaseHTTPRequestHandler):
   \____\_______/
 
 
-Hello from Docker first push and Nashir is your unkul!
+Hello from Docker first push and Nashir is your unkul!!!
 ''')
 
 def run():
